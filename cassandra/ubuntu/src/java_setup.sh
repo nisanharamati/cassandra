bd2506f89ceb1f9a7eb9a7ab6c091870e2b27343
nisanharamati <hanisan@gmail.com>
2014-06-03 01:25:56 +0000

    remove java_setup.sh

--- cassandra/ubuntu/src/java_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir /usr/lib/jvm
-tar -zxvf jre-7u51-linux-x64.gz -C /usr/lib/jvm
-update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jre1.7.0_51/bin/java" 1
-update-alternatives --set java /usr/lib/jvm/jre1.7.0_51/bin/java
-java -version
-
-# get Java Native Access (JNA)
-apt-get -y -f install libjna-java
