bc387004a17c7ed6ab1984af9972018072829a19
nisanharamati <hanisan@gmail.com>
2014-06-11 23:00:24 +0000

    extended scripts/run.sh to support image tag\nupdated cassandra:ubuntu start.sh to not touch the configuration files.

--- cassandra/ubuntu/src/start.sh
+++ cassandra/ubuntu/src/start.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-IP=`hostname --ip-address`
-if [ $# == 1 ]; then SEEDS="$1,$IP";
-else SEEDS="$IP"; fi
+#IP=`hostname --ip-address`
+#if [ $# == 1 ]; then SEEDS="$1,$IP";
+#else SEEDS="$IP"; fi
 
-echo Configuring Cassandra to listen at $IP with seeds $SEEDS
+#echo Configuring Cassandra to listen at $IP with seeds $SEEDS
 
 # Setup Cassandra
-DEFAULT=/etc/cassandra/default.conf
-CONFIG=/etc/cassandra/conf
+#DEFAULT=/etc/cassandra/default.conf
+#CONFIG=/etc/cassandra/conf
 
-rm -rf $CONFIG && cp -r $DEFAULT $CONFIG
-sed -i -e "s/^listen_address.*/listen_address: $IP/"            $CONFIG/cassandra.yaml
-sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"              $CONFIG/cassandra.yaml
-sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/"       $CONFIG/cassandra.yaml
-sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
+#rm -rf $CONFIG && cp -r $DEFAULT $CONFIG
+#sed -i -e "s/^listen_address.*/listen_address: $IP/"            $CONFIG/cassandra.yaml
+#sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"              $CONFIG/cassandra.yaml
+#sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/"       $CONFIG/cassandra.yaml
+#sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
 
 # Start process
-echo Starting Cassandra on $IP...
+#echo Starting Cassandra on $IP...
+
+# start supervisord which will start cassandra
 /usr/bin/supervisord
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,16 +5,26 @@ cd $(dirname $0)
 HOW_MANY=${1}
 PREFIX=${2-cass}
 IMAGE=${3-poklet/cassandra}
+TAG=${4-}
+if [ "$TAG" != "" ]
+then # IF length of $TAG is non-zero, prepend a ':' to it
+	IMAGE="$IMAGE:$TAG"
+fi
 
 if [[ ${#@} = 0 ]]; then
   echo Runs multiple Cassandra containers
-  echo usage: $0 NUMBER-OF-CONTAINERS [HOSTNAME-PREFIX] [IMAGE-NAME]
+  echo usage: $0 NUMBER-OF-CONTAINERS [HOSTNAME-PREFIX] [IMAGE-NAME] [IMAGE-TAG]
   echo
-  echo Defaults: HOSTNAME-PREFIX:$PREFIX, IMAGE-NAME:$IMAGE
+  echo Defaults: HOSTNAME-PREFIX:$PREFIX, IMAGE-NAME:$IMAGE IMAGE-TAG:$TAG
   echo Example: $0 3      # => Runs 3 Cassandra container called cass1, cass2 & cass3
   echo Example: $0 1 demo # => Runs 1 Cassandra container called demo1
+  echo Example: $0 2 cass poklet/cassandra latest # => Runs 2 Cassandra containers called cass1 cass2 built with docker image poklet/cassandra:latest
   exit 1
 fi
+
+echo Instances: $HOW_MANY
+echo PREFIX: $PREFIX
+echo IMAGE: $IMAGE
 
 docker run -d --name ${PREFIX}1 $IMAGE
 SEED=$(./ipof.sh ${PREFIX}1)
