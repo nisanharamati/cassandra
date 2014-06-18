196cef03ab2c53d00797606c9b9e1deff6bb347e
nisanharamati <hanisan@gmail.com>
2014-06-18 00:40:00 +0000

    .

--- cassandra/ubuntu/src/start.sh
+++ cassandra/ubuntu/src/start.sh
@@ -18,6 +18,10 @@
 
 # Start process
 #echo Starting Cassandra on $IP...
+# change ownership to cassandra on the configs, volumes, and logs
+chown -R cassandra:cassandra /var/lib/cassandra
+chown -R cassandra:cassandra /etc/cassandra
+chown -R cassandra:cassandra /var/log/cassandra
 
 # start supervisord which will start cassandra
 /usr/bin/supervisord
