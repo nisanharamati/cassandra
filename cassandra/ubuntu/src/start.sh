96ea85d131967602514f9c8c3fae99a5b384bf5f
unknown <nisan@POF-Nisan.pof.local>
2014-07-18 16:18:48 -0700

    lock versions, remove crud

--- cassandra/ubuntu/src/start.sh
+++ cassandra/ubuntu/src/start.sh
@@ -1,28 +1,10 @@
 #!/usr/bin/env bash
 
-#IP=`hostname --ip-address`
-#if [ $# == 1 ]; then SEEDS="$1,$IP";
-#else SEEDS="$IP"; fi
-
-#echo Configuring Cassandra to listen at $IP with seeds $SEEDS
-
-# Setup Cassandra
-#DEFAULT=/etc/cassandra/default.conf
-#CONFIG=/etc/cassandra/conf
-
-#rm -rf $CONFIG && cp -r $DEFAULT $CONFIG
-#sed -i -e "s/^listen_address.*/listen_address: $IP/"            $CONFIG/cassandra.yaml
-#sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"              $CONFIG/cassandra.yaml
-#sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/"       $CONFIG/cassandra.yaml
-#sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
-
-# Start process
-#echo Starting Cassandra on $IP...
 # change ownership to cassandra on the configs, volumes, and logs
 chown -R cassandra:cassandra /var/lib/cassandra
 chown -R cassandra:cassandra /etc/cassandra
 chown -R cassandra:cassandra /var/log/cassandra
 
+
 # start supervisord which will start cassandra
 /usr/bin/supervisord
-
